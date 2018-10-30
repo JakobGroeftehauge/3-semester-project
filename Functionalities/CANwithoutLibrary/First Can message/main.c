b1f4f77d8e91a945e7332c8b8cd87e0f1e7288c5
Christian Eberhardt <chebe17@student.sdu.dk>
2018-10-30 08:49:45 +0100

    Added TransmitReceive, adjusted First Can Message

--- Functionalities/CANwithoutLibrary/First Can message/main.c
+++ Functionalities/CANwithoutLibrary/First Can message/main.c
@@ -7,6 +7,7 @@
 
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/delay.h>
 
 #define bit_get(p,m) ((p) & (m))
 #define bit_set(p,m) ((p) |= (m))
@@ -15,8 +16,9 @@
 #define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m))
 #define BIT(x) (0x01 << (x))
 #define LONGBIT(x) ((unsigned long)0x00000001 << (x))
+#define F_CPU 8000000UL
 
-
+void extra_delay(uint8_t delaytimes);
  
 
 
@@ -30,6 +32,8 @@ int main(void)
 	bit_set(DDRD, BIT(7));
 	
 	bit_set(PORTD, BIT(7));
+	
+	int8_t hello = 1;
 
 //***** CAN initialization *****************************************************
 
@@ -37,9 +41,9 @@ int main(void)
 
 	CANTCON = 0x00;         // CAN timing prescaler set to 0;
 
-	CANBT1 = 0x0E;      	// Set baud rate to 125kb (assuming 8Mhz IOclk)
-	CANBT2 = 0x04;          // "" See page 240 in the datasheet
-	CANBT3 = 0x13; 			// ""
+	CANBT1 = 0x02;      	// Set baud rate to 250kb (assuming 8Mhz IOclk)
+	CANBT2 = 0x0C;          // "" See page 240 in the datasheet
+	CANBT3 = 0x37; 			// ""
 
 	for ( int8_t mob=0; mob<6; mob++ ) {
 
@@ -84,6 +88,27 @@ int main(void)
 		CANMSG = 0x88;
 		
 		CANCDMOB = ( 1 << CONMOB0 ) | ( 1 << DLC0 ); 	// Enable transmission, data length=1 (CAN Standard rev 2.0A)
+		
+		extra_delay(20);
+		
+		/*if (hello == 1)
+		{
+			bit_set(PORTD, BIT(1));
+			hello = 0;
+		}else{
+			bit_clear(PORTD,BIT(1));
+			hello = 1;
+		}*/
+		bit_flip(PORTD, BIT(1));
+		
+		
+		
+		
     }
 }
 
+void extra_delay(uint8_t delaytimes){
+	for (uint8_t i = delaytimes; i > 0; i--){
+		_delay_ms(20);
+	}
+}
