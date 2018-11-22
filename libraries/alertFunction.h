7977e9664e06bf27873ab87fbe302223f515ee00
JakobGroeftehauge <jakra17@student.sdu.dk>
2018-11-22 18:46:49 +0100

    alertFunction added

---
+++ libraries/alertFunction.h
@@ -0,0 +1,27 @@
+/*
+ * alertFunction.h
+ *
+ * Created: 11/21/2018 12:52:11 PM
+ *  Author: Jakob
+ */ 
+
+//#include "alertFunction.c"
+
+
+#ifndef ALERTFUNCTION_H_
+#define ALERTFUNCTION_H_
+
+
+
+//void (*foo)(void); 
+
+//void setupAlertFunction(void (*ptr))
+
+extern void setupAlertFunction();
+
+extern void alertFunction(); 
+
+extern void blinkLED(); 
+
+
+#endif /* ALERTFUNCTION_H_ */
