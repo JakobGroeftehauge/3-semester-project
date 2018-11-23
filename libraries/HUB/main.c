b306ea310c2ab6f02f7b68730255fc3b2aa1f61a
kulus96 <larpe17@student.sdu.dk>
2018-11-23 11:16:34 +0100

    Update main.c

--- libraries/HUB/main.c
+++ libraries/HUB/main.c
@@ -71,57 +71,56 @@ heartBeatSCS = 0;
 
 
 //volatile sensorData sensorList[NUMBER_OF_SENSOR];
-floatUnion coefList1[4]; 
+floatUnion coefList1[2];
 floatUnion coefList2[2];
 floatUnion coefList3[2];
 
 
-coefList1[0].floatVal = 3; 
-coefList1[1].floatVal = 40; 
-coefList1[2].floatVal = 23.4; 
-coefList1[3].floatVal = 204; 
+coefList1[0].floatVal = 23.545;
+coefList1[1].floatVal = 343.214;
 
-coefList2[0].floatVal = 1;
-coefList2[1].floatVal = 0;
+coefList2[0].floatVal = 19.545;
+coefList2[1].floatVal = 343.214;
 
-coefList3[0].floatVal = 1;
-coefList3[1].floatVal = 0;
+coefList3[0].floatVal = 300.545;
+coefList3[1].floatVal = 343.214;
 //Setup sensorData structs
 
 sensorList[0].sensorStruct.CAN_ID = 0x0001;
-sensorList[0].sensorStruct.samplingfreq = 2; 
+sensorList[0].sensorStruct.samplingfreq = 2;
 sensorList[0].sensorStruct.period = 100;
-sensorList[0].sensorStruct.cutOffFreq = 2; 
-sensorList[0].sensorStruct.unit = celsius; 
+sensorList[0].sensorStruct.cutOffFreq = 2;
+sensorList[0].sensorStruct.unit = celsius;
 sensorList[0].sensorStruct.sensor_Type = other_sensor;
-sensorList[0].sensorStruct.totalNumberOfpolynomials = 4;
-sensorList[0].sensorStruct.polynomialList = &coefList1[0]; 
+sensorList[0].sensorStruct.totalNumberOfpolynomials = 2;
+sensorList[0].sensorStruct.polynomialList = &coefList1[0];
 sensorList[0].ACK = 0;
 sensorList[0].data = 0;
-sensorList[0].isSCS = 1; 
+sensorList[0].isSCS = 1;
 
-// sensorList[1].sensorStruct.CAN_ID = 0x0002;
-// sensorList[1].sensorStruct.samplingfreq = 2;
-// sensorList[1].sensorStruct.period = 0; 
-// sensorList[1].sensorStruct.cutOffFreq = 2; 
-// sensorList[1].sensorStruct.unit = degrees;
-// sensorList[1].sensorStruct.sensor_Type = thermistor;
-// sensorList[1].sensorStruct.totalNumberOfpolynomials = 2;
-// sensorList[1].sensorStruct.polynomialList = &coefList2[0]; 
-// sensorList[1].data = 0;
-// sensorList[1].ACK = 0;  
-// sensorList[1].isSCS = 0;
-// 
-// sensorList[2].sensorStruct.CAN_ID = 0x00CC;
-// sensorList[2].sensorStruct.samplingfreq = 2;
-// sensorList[2].sensorStruct.period = 2; 
-// sensorList[2].sensorStruct.cutOffFreq = 2; 
-// sensorList[2].sensorStruct.unit = percentage;
-// sensorList[2].sensorStruct.sensor_Type = potentiometer;
-// sensorList[2].sensorStruct.totalNumberOfpolynomials = 2;
-// sensorList[2].sensorStruct.polynomialList = &coefList3[0]; 
-// sensorList[2].data = 0;
-// sensorList[2].isSCS = 0;
+
+sensorList[1].sensorStruct.CAN_ID = 0x0002;
+sensorList[1].sensorStruct.samplingfreq = 2;
+sensorList[1].sensorStruct.period = 100;
+sensorList[1].sensorStruct.cutOffFreq = 2;
+sensorList[1].sensorStruct.unit = degrees;
+sensorList[1].sensorStruct.sensor_Type = thermistor;
+sensorList[1].sensorStruct.totalNumberOfpolynomials = 2;
+sensorList[1].sensorStruct.polynomialList = &coefList2[0];
+sensorList[1].data = 0;
+sensorList[1].ACK = 0;
+sensorList[1].isSCS = 0;
+
+sensorList[2].sensorStruct.CAN_ID = 0x00CC;
+sensorList[2].sensorStruct.samplingfreq = 2;
+sensorList[2].sensorStruct.period = 2;
+sensorList[2].sensorStruct.cutOffFreq = 2;
+sensorList[2].sensorStruct.unit = percentage;
+sensorList[2].sensorStruct.sensor_Type = potentiometer;
+sensorList[2].sensorStruct.totalNumberOfpolynomials = 2;
+sensorList[2].sensorStruct.polynomialList = &coefList3[0];
+sensorList[2].data = 0;
+sensorList[2].isSCS = 0;
 
 
 bit_set(PORTD, BIT(1));
@@ -214,7 +213,8 @@ ISR( CAN_INT_vect )
 		}
 		case 0xA0: //ERROR SIGNAL 
 		{
-			//NOT DEFINED - CALL BACk
+			alertFunction();
+			break;//ERROR SIGNAL
 		}
 		case 0xC8: // Sensor Requester
 		{
